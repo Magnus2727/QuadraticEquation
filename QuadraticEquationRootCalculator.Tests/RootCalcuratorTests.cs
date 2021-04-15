752d1275625855ca29688fec694979edc8ac40cb
Magnus2727 <mikolaj_koczur@wp.pl>
2021-04-15 16:33:16 +0200

    Add project files.

---
+++ QuadraticEquationRootCalculator.Tests/RootCalcuratorTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuadraticEquationRootCalculator.Tests
+{
+    [TestFixture]
+    public class RootCalcuratorTests
+    {
+        [Test]
+        [TestCase(1,1,-1)]
+        [Ignore("copy paste template")]
+        public void testTemplate(double firstValue, double secondValue, double thirdValue)
+        {
+            RootCalculator rc = new RootCalculator(firstValue, secondValue, thirdValue);
+            rc.RootCalculate();
+            var result = rc.Delta;
+            Assert.Greater(result, 0);
+        }
+    }
+}
---
+++ QuadraticEquationRootCalculator/RootCalculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuadraticEquationRootCalculator
+{
+    public class RootCalculator
+    {
+        public double Root1 { get; private set; }
+        public double Root2 { get; private set; }
+        public double Delta { get; private set; }
+        private double A { get; set; }
+        private double B { get; set; }
+        private double C { get; set; }
+
+        public RootCalculator(double a, double b, double c)
+        {
+            this.A = a;
+            this.B = b;
+            this.C = c;
+        }
+
+        public void RootCalculate()
+        {
+
+            if (A == 0)
+            {
+                Console.WriteLine("function is linear, no roots");
+                return;
+            }
+
+            CalculateDelta();
+            double divider = 2 * A;
+
+            if (Delta > 0)
+            {
+                Root1 = (-B / divider) + (Math.Sqrt(Delta) / divider);
+                Root2 = (-B / divider) - (Math.Sqrt(Delta) / divider);
+                Console.WriteLine($"function with two roots: {Root1}, {Root2}");
+                return;
+            }
+
+            if (Delta == 0)
+            {
+                Root1 = -B / divider;
+                Root2 = Root1;
+                Console.WriteLine($"function with one root: {Root1}");
+                return;
+            }
+
+            if (Delta < 0)
+            {
+                Console.WriteLine("Equation without real roots");
+            }
+
+        }
+        private void CalculateDelta()
+        {
+            Delta = Math.Pow(B, 2) - (4 * A * C);
+        }
+    }
+}
