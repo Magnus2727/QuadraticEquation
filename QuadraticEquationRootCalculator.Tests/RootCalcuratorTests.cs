64bd37ffedf3e4805f34040abbe9c48a4a1d315b
Magnus2727 <mikolaj_koczur@wp.pl>
2021-04-16 13:35:18 +0200

    added two more test for delta 0 and 1 root

--- QuadraticEquationRootCalculator.Tests/RootCalcuratorTests.cs
+++ QuadraticEquationRootCalculator.Tests/RootCalcuratorTests.cs
@@ -59,5 +59,22 @@ namespace QuadraticEquationRootCalculator.Tests
             rc.RootCalculate();
             Assert.AreEqual(rc.Roots.Count, 2);
         }
+        [Test]
+        [TestCase(1, -2, 1)]
+        public void CalculateQuadraticEquationRoots_DeltaEqualsZero_OneRoot(double firstValue, double secondValue, double thirdValue)
+        {
+            RootCalculator rc = new RootCalculator(firstValue, secondValue, thirdValue);
+            rc.RootCalculate();
+            Assert.AreEqual(rc.Roots.Count, 1);
+        }
+        [Test]
+        [TestCase(1, -2, 1)]
+        public void CalculateQuadraticEquationRoots_ExampleWithDeltaEqualsZero_DelatEqualsZero(double firstValue, double secondValue, double thirdValue)
+        {
+            RootCalculator rc = new RootCalculator(firstValue, secondValue, thirdValue);
+            rc.RootCalculate();
+            Assert.AreEqual(rc.Delta, 0);
+        }
+
     }
 }
