9296b6b746fd8005ca69c0a2de13e8572d677251
Magnus2727 <mikolaj_koczur@wp.pl>
2021-04-16 13:31:51 +0200

    Added test for delta higher than 0 and root count equals 2

--- QuadraticEquationRootCalculator.Tests/RootCalcuratorTests.cs
+++ QuadraticEquationRootCalculator.Tests/RootCalcuratorTests.cs
@@ -11,12 +11,53 @@ namespace QuadraticEquationRootCalculator.Tests
         [Test]
         [TestCase(1,1,-1)]
         [Ignore("copy paste template")]
-        public void testTemplate(double firstValue, double secondValue, double thirdValue)
+        public void CalculateQuadraticEquationRoots_(double firstValue, double secondValue, double thirdValue)
         {
             RootCalculator rc = new RootCalculator(firstValue, secondValue, thirdValue);
             rc.RootCalculate();
             var result = rc.Delta;
             Assert.Greater(result, 0);
+        }
+
+        [Test]
+        [TestCase(1, 1, -1)]
+        [TestCase(1, 0, -1)]
+        [TestCase(1, -1, -1)]
+        public void CalculateQuadraticEquationRoots_PositiveFirstNegativeThirdValue_DeltaAlwaysHigherThanZero(double firstValue, double secondValue, double thirdValue)
+        {
+            RootCalculator rc = new RootCalculator(firstValue, secondValue, thirdValue);
+            rc.RootCalculate();
+            Assert.Greater(rc.Delta, 0);
+        }
+        [Test]
+        [TestCase(1, 1, -1)]
+        [TestCase(1, 0, -1)]
+        [TestCase(1, -1, -1)]
+        public void CalculateQuadraticEquationRoots_DeltaHigherThanZero_TwoRootsExpected(double firstValue, double secondValue, double thirdValue)
+        {
+            RootCalculator rc = new RootCalculator(firstValue, secondValue, thirdValue);
+            rc.RootCalculate();
+            Assert.AreEqual(rc.Roots.Count, 2);
+        }
+        [Test]
+        [TestCase(-1, 1, 1)]
+        [TestCase(-1, 0, 1)]
+        [TestCase(-1, -1, 1)]
+        public void CalculateQuadraticEquationRoots_NegativeFirstPositiveThirdValue_DeltaAlwaysHigherThanZero(double firstValue, double secondValue, double thirdValue)
+        {
+            RootCalculator rc = new RootCalculator(firstValue, secondValue, thirdValue);
+            rc.RootCalculate();
+            Assert.Greater(rc.Delta, 0);
+        }
+        [Test]
+        [TestCase(-1, 1, 1)]
+        [TestCase(-1, 0, 1)]
+        [TestCase(-1, -1, 1)]
+        public void CalculateQuadraticEquationRoots_NegativeFirstPositiveThirdValue_TwoRootsExpected(double firstValue, double secondValue, double thirdValue)
+        {
+            RootCalculator rc = new RootCalculator(firstValue, secondValue, thirdValue);
+            rc.RootCalculate();
+            Assert.AreEqual(rc.Roots.Count, 2);
         }
     }
 }
